96cb03c43ecf6d89117e323237e12d3322a643ca
simonloeffler <s.loeffler@hs-furtwangen.de>
2018-03-28 22:33:31 +0200

    praktikum

---
+++ Aufgabe1/1.1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Aufgabe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                var type = args[0];
+                var size = double.Parse(args[1]);
+                double surface = 0;
+                double volume = 0;
+                string info = "";
+                if(type != null && size != 0)
+                    switch(type){
+                        case "w":
+                            surface=getCubeSurface(size);
+                            volume=getCubeVolume(size);
+                            info = GetCubeInfo(surface, volume);
+                            break;
+                        case "k":
+                            surface=getBallSurface(size);
+                            volume=getBallVolume(size);
+                            info = GetBallInfo(surface, volume);
+                            break;
+                        case "o":
+                            surface=getOctSurface(size);
+                            volume=getOctVolume(size);
+                            info = GetOctInfo(surface, volume);
+                            break;
+                    }
+                
+                Console.WriteLine(info);
+
+            } catch(Exception){
+                Console.WriteLine("Eingabe fehlerhaft");
+            }
+
+        }
+
+        public static string GetCubeInfo(double surface, double volume){
+            return "Würfel: A=" + Math.Round(surface, 2) + " | V=" + Math.Round(volume, 2);
+        }
+
+        public static string GetBallInfo(double surface, double volume){
+            return "Kugel: A=" + Math.Round(surface, 2) + " | V=" + Math.Round(volume, 2);
+        }
+
+        public static string GetOctInfo(double surface, double volume){
+            return "Oktaeder: A=" + Math.Round(surface, 2) + " | V=" + Math.Round(volume, 2);
+        }
+
+        public static double getCubeSurface(double size){
+            return 6*Math.Pow(size, 2);
+        }
+        public static double getCubeVolume(double size){
+            return Math.Pow(size, 3);
+        }
+
+        public static double getBallSurface(double size){
+            return Math.PI * Math.Pow(size, 2);
+        }
+        public static double getBallVolume(double size){
+            return (Math.PI * Math.Pow(size, 3))/6;
+        }
+
+        public static double getOctSurface(double size){
+            return 2*Math.Sqrt(3)*Math.Pow(size, 2);
+        }
+        public static double getOctVolume(double size){
+            return (Math.Sqrt(2)*Math.Pow(size, 3))/3;
+        }
+    }
+}
---
+++ Aufgabe1/1.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1._2
+{
+    class Program
+    {
+
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+
+        static void Main(string[] args)
+        {
+            string[] poem = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                poem[i] = GetVerse();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(poem[i]);
+            }
+        }
+
+        public static string GetVerse()
+        {
+            Random rnd = new Random();
+            string verse = subjects[rnd.Next(subjects.Length - 1)] + " " + verbs[rnd.Next(verbs.Length - 1)] + " " + objects[rnd.Next(objects.Length - 1)];
+            return verse;
+        }
+    }
+}
