bece1afb9be86263142eb170969aba7b8b62b1b9
simonloeffler <s.loeffler@hs-furtwangen.de>
2018-04-12 11:46:44 +0200

    l03

---
+++ L03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace L03
+{
+
+    public class Person{
+        public string FirstName;
+        public string LastName;
+        public int Age;
+
+        public override string ToString() 
+        {
+            return ($"{FirstName} {LastName} ist {Age} Jahre alt.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] i = new int[] {5,6,2};
+            Person [] personen =
+            {
+                new Person {FirstName = "Der", LastName = "Dude", Age = 42 },
+                new Person {FirstName = "Stuntman", LastName = "Mike", Age = 53 }
+            };
+
+            GetPersonen(personen);
+            //Console.WriteLine();
+        }
+
+        public static void GetPersonen (Person[] personen){
+            for(int i = 0; i<personen.Length; i++)
+                Console.WriteLine(personen[i]);
+        }
+    }
+}
---
+++ L03_Debugging/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+public class SimplePerson
+{
+   public string FirstName;
+   public string LastName;
+   public DateTime DateOfBirth;
+}
+
+namespace Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person root = Familytree.BuildTree();
+
+            Person found = Familytree.FindPersonByAge(root, 100, 110);
+
+            WriteLine(found);
+        }
+    }
+}
