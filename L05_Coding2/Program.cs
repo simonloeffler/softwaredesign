ab74cf10f3ef51f329bf8f522a91c4c990189893
simonloeffler <s.loeffler@hs-furtwangen.de>
2018-04-28 10:08:31 +0200

    commit from laptop

---
+++ L05_Coding2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L05_Coding2
+{
+    class Person
+    {
+        public string Name;
+        public int Alter;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person einePerson = new Person
+            {
+                Name = "Heiner",
+                Alter = 42
+            };
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
